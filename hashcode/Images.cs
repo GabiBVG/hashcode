09d81df65b4f30558a0cc9ac31b6d55b1bfd6fe1
Bizdoc <gabriel.bizoc@ligaac.ro>
2019-02-28 20:25:35 +0200

    create dataset

---
+++ hashcode/Images.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashcode
+{
+    class Images
+    {
+        public List<string> tags { get; set; }
+        public string orientation { get; set; }
+
+        public Images( string o)
+        {
+            orientation = o;
+            this.tags = new List<string>();
+        }
+        public void AddTag(string tag)
+        {
+            tags.Add(tag);
+        }
+        public Images this[int index]
+        {
+            get { return this; }
+        }
+    }
+}
--- hashcode/Program.cs
+++ hashcode/Program.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace hashcode
 {
     class Program
     {
+        class dataset {
+
+        }
+        static List<Images> dataInput = new List<Images>();
         static void Main(string[] args)
         {
+
+            readDAtaset();
+            var plm = dataInput[5];
+        }
+
+        static void readDAtaset()
+        {
+            string nume = Directory.GetCurrentDirectory();
+            string path = @"..\..\Data\b_lovely_landscapes.txt";
+            string[] files = File.ReadAllLines(path);
+            for(int i=1; i< Int32.Parse(files[0]); ++i)
+            {
+                string[] content = files[i].Split(' ');
+                var new_img = new Images(content[0]);
+                for (var j = 2; j < content.Length; ++j)
+                {
+                    new_img.AddTag(content[j]);
+                }
+                dataInput.Add(new_img);
+
+            }
+
+            //     foreach (File in files;)
         }
     }
 }
